12472795ea0ad081133d239d9abb25a6e8a3e81d
Marcio Castillo <mecastillo00@gmail.com>
2021-05-15 17:37:19 -0400

    added env file for port number

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-app.listen(9000, () => console.log("listening on port 9000"));
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
